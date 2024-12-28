49dad9a4043a32b693e82ab0db602d640f139683
Shohnurdev <abdugaffarovshohnur1@gmail.com>
2024-12-28 18:31:50 +0500

    ilk_dastur qo'shildi

---
+++ ilk_dastur.py
@@ -0,0 +1,9 @@
+a = int(input("1-> Akkaunt yaratish \n2->Akkauntga kirish -> \n-> "))
+if a == 1 : 
+    gmail = input("Email pochtangini kiriting -> ")
+    kod = input("Email pochtangizga kelgan kodni kiriting -> ")
+elif a ==2 :
+    login1 = input("Loginizni kiriting -> ")
+    parol1 = input("Parolizni kiriting -> ")
+else :
+    print("Iltimos 1 yoki 2 raqamini kiriting")
